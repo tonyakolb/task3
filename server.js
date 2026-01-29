9cc7b6db984a720417e1cb1231273f6224b049f5
tonyakolb <antoninakolb@gmail.com>
2026-01-29 16:29:59 +0300

    add files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+function isNaturalNumber(num) {
+    const n = Number(num);
+    return Number.isFinite(n) && Number.isInteger(n) && n > 0;
+}
+
+function gcd(a, b) {
+    while (b !== 0) {
+        const temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return Math.abs(a * b) / gcd(a, b);
+}
+
+app.get('/antoninakolb_gmail_com', (req, res) => {
+    const x = req.query.x;
+    const y = req.query.y;
+    
+    if (x === undefined || y === undefined) {
+        return res.send('NaN');
+    }
+    
+    if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
+        return res.send('NaN');
+    }
+    
+    const numX = Number(x);
+    const numY = Number(y);
+    const result = lcm(numX, numY);
+    
+    res.send(result.toString());
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
